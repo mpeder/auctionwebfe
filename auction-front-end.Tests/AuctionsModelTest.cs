e07bf8e764f21f9b096c4546254e5fc09151b753
Anders Lybecker <anders@lybecker.com>
2018-09-13 14:26:29 +0200

    Sln file and simple unit test added

---
+++ auction-front-end.Tests/AuctionsModelTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using auction_front_end.Pages;
+
+namespace auction_front_end.Tests
+{
+    [TestClass]
+    public class AuctionsModelTest
+    {
+        [TestMethod]
+        public void OnGet_Returns_Message()
+        {
+            // Arrange
+            var sut = new AuctionsModel();
+
+            // Act
+            sut.OnGet();
+
+            // Assert
+            StringAssert.Contains(sut.Message, "auctions");
+        }
+    }
+}
